6601582223e5697d90716d19be85cabb45dedaf0
taxeric <51876689+taxeric@users.noreply.github.com>
2023-01-15 16:14:34 +0800

    [test]: ternary operators

--- main.cpp
+++ main.cpp
@@ -8,11 +8,14 @@ void printClassName(Printable * p) {
     cout << p->getClassName() << endl;
 }
 
+static int s_Level = 0;
+static int s_Speed = 2;
+
 int main() {
-    MapEntity e = MapEntity();
-    e.setX(101);
-    cout << e.getX() << endl;
-    cout << e.getNameV1() << endl;
-    e.print();
+    srand(time(nullptr));
+    s_Level = ::rand() % 20;
+    s_Speed = s_Level > 5 ? 10 : 5;
+    string str = s_Level > 10 ? "A" : "B";
+    cout << s_Level << endl << s_Speed << endl << str;
     return 0;
 }
